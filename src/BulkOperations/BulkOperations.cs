4281dcc8be9c275ac8a87d0899b7979c037ba8ec
orxan.nagiyev <orxan.naghiyev@abc-telecom.az>
2023-07-06 09:31:46 +0400

    Add project files.

---
+++ src/BulkOperations/BulkOperations.cs
@@ -0,0 +1,122 @@
+﻿using LaRoy.Mapper.BulkOperations.Utils;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace LaRoy.BulkOperations
+{
+    public static partial class BulkOperations
+    {
+        public static int BulkInsert<T>(this SqlConnection connection, IEnumerable<T> data)
+        {
+            if (connection.State != ConnectionState.Open)
+                connection.Open();
+
+            var sw = new Stopwatch();
+            sw.Start();
+            using (SqlTransaction transaction = connection.BeginTransaction())
+            {
+                try
+                {
+                    var affRows = 0;
+                    using (SqlBulkCopy bulkCopy = new SqlBulkCopy(connection, SqlBulkCopyOptions.Default, transaction))
+                    {
+                        bulkCopy.DestinationTableName = typeof(T).Name;
+                        DataTable dataTable = new DataTable();
+                        var properties = typeof(T).GetProperties();
+
+                        foreach (var property in properties)
+                        {
+                            dataTable.Columns.Add(property.Name, Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType);
+                            bulkCopy.ColumnMappings.Add(property.Name, property.Name);
+                        }
+
+                        foreach (var item in data)
+                        {
+                            var values = new object[properties.Length];
+                            for (int i = 0; i < properties.Length; i++)
+                            {
+                                values[i] = properties[i].GetValue(item);
+                            }
+                            dataTable.Rows.Add(values);
+                        }
+
+                        bulkCopy.WriteToServer(dataTable);
+                        affRows = dataTable.Rows.Count;
+                    }
+                    transaction.Commit();
+                    sw.Stop();
+                    Console.WriteLine(sw.Elapsed);
+                    return affRows;
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw ex;
+                }
+            }
+        }
+
+        public static int BulkInsert<T>(this LaRoyDbContext context, IEnumerable<T> data)
+        {
+            var connection = context.GetConnection();
+            return BulkInsert(connection, data);
+        }
+
+        public static int BulkInsert<T>(this SqlConnection connection, T[] data)
+        {
+            if (connection.State != ConnectionState.Open)
+                connection.Open();
+
+            var sw = new Stopwatch();
+            sw.Start();
+            using (SqlTransaction transaction = connection.BeginTransaction())
+            {
+                try
+                {
+                    var affRows = 0;
+                    using (SqlBulkCopy bulkCopy = new SqlBulkCopy(connection, SqlBulkCopyOptions.Default, transaction))
+                    {
+                        bulkCopy.DestinationTableName = typeof(T).Name;
+                        DataTable dataTable = new DataTable();
+                        var properties = typeof(T).GetProperties();
+
+                        foreach (var property in properties)
+                        {
+                            dataTable.Columns.Add(property.Name, Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType);
+                            bulkCopy.ColumnMappings.Add(property.Name, property.Name);
+                        }
+
+                        foreach (var item in data)
+                        {
+                            var values = new object[properties.Length];
+                            for (int i = 0; i < properties.Length; i++)
+                            {
+                                values[i] = properties[i].GetValue(item);
+                            }
+                            dataTable.Rows.Add(values);
+                        }
+
+                        bulkCopy.WriteToServer(dataTable);
+                        affRows = dataTable.Rows.Count;
+                    }
+                    transaction.Commit();
+                    sw.Stop();
+                    Console.WriteLine(sw.Elapsed);
+                    return affRows;
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw ex;
+                }
+            }
+        }
+
+        public static int BulkInsert<T>(this LaRoyDbContext context, T[] data)
+        {
+            var connection = context.GetConnection();
+            return BulkInsert(connection, data);
+        }
+    }
+}
---
+++ src/Utils/LaRoyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaRoy.Mapper.BulkOperations.Utils
+{
+    public abstract class LaRoyDbContext
+    {
+        private readonly SqlConnection _connection;
+        public LaRoyDbContext(string connectionString)
+        {
+                _connection = new SqlConnection(connectionString);
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return _connection;
+        }
+    }
+}
